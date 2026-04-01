d373fb7155f4622d8cb984b07c840155815626cd
renatorms <rramosmachados16@gmail.com>
2026-04-01 10:31:41 -0300

    feat: enhance admin with inline variations and filters

--- backend/produtos/admin.py
+++ backend/produtos/admin.py
@@ -1,7 +1,37 @@
 from django.contrib import admin
 
-from .models import Categoria, Produto, Variacao
+from .models import Categoria, Marca, Produto, Variacao
 
-admin.site.register(Categoria)
-admin.site.register(Produto)
-admin.site.register(Variacao)
+
+@admin.register(Marca)
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = ["nome"]
+    search_fields = ["nome"]
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ["nome"]
+    search_fields = ["nome"]
+
+
+class VariacaoInline(admin.TabularInline):
+    model = Variacao
+    extra = 3
+    fields = ["tamanho", "estoque"]
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ["nome", "preco", "marca", "categoria"]
+    list_filter = ["categoria", "marca"]
+    search_fields = ["nome", "marca__nome"]
+    inlines = [VariacaoInline]
+
+
+@admin.register(Variacao)
+class VariacaoAdmin(admin.ModelAdmin):
+    list_display = ["produto", "tamanho", "estoque"]
+    list_filter = ["produto__categoria", "tamanho"]
+    search_fields = ["produto__nome"]
+    list_editable = ["estoque"]
