6297064b04c83c3c38e89309f12ed32c9db84fd9
renatorms <rramosmachados16@gmail.com>
2026-07-11 22:21:56 -0300

    feat: add IsAuthenticatedOrReadOnly to product ViewSets

--- backend/produtos/views.py
+++ backend/produtos/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Categoria, Marca, Produto, Variacao
 from .serializers import (
@@ -12,16 +13,19 @@ from .serializers import (
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class MarcaViewSet(viewsets.ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class VariacaoViewSet(viewsets.ModelViewSet):
     queryset = Variacao.objects.all()
     serializer_class = VariacaoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class ProdutoViewSet(viewsets.ModelViewSet):
@@ -29,6 +33,7 @@ class ProdutoViewSet(viewsets.ModelViewSet):
         "variacoes"
     )
     serializer_class = ProdutoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filterset_fields = ["marca", "categoria"]
     search_fields = ["nome", "marca__nome"]
     ordering_fields = ["nome", "preco"]
