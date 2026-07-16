8cebe86a9e0b71a0e24497e468234e730184ea3c
renatorms <rramosmachados16@gmail.com>
2026-07-15 21:56:22 -0300

    fix: add unique constraint to Categoria.nome

--- backend/produtos/models.py
+++ backend/produtos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Categoria(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.nome
