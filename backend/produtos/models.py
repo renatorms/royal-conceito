1c603079860211181dd688429eb138fcd68db8ca
renatorms <rramosmachados16@gmail.com>
2026-07-15 22:02:41 -0300

    feat: add default ordering to Categoria

--- backend/produtos/models.py
+++ backend/produtos/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=100, unique=True)
 
+    class Meta:
+        ordering = ["nome"]
+
     def __str__(self):
         return self.nome
 
